c7a74d11dec6bf862d9152dcb9ab2cc56fd50697
Reni Calonge <rdccalonge@gmail.com>
2024-01-12 18:32:57 +0800

    RainfallService unit test.

---
+++ RainfallApi.Tests/RainfallApi.Application.Tests/RainfallServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using RainfallApi.Application.Services;
+using RainfallApi.Core.Entities;
+using Xunit;
+
+namespace RainfallApi.Tests.RainfallApi.Application.Tests
+{
+    public class RainfallServiceTests
+    {
+        [Fact]
+        public async Task GetRainfallReadingsAsync_ShouldReturnReadings()
+        {
+            // Arrange
+            var service = new RainfallService();
+
+            // Act
+            List<RainfallReading> result = await service.GetRainfallReadingsAsync("stationId");
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Single(result);
+        }
+    }
+}
