cefe175a1071e877bf86da15da479825f1a55d31
Reni Calonge <rdccalonge@gmail.com>
2024-01-12 19:29:48 +0800

    Implement Rainfall API client

---
+++ RainfallApi.Infrastructure/Clients/RainfallApiClient.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using RainfallApi.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallApi.Infrastructure.Clients
+{
+    public class RainfallApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IMapper _mapper;
+
+        public RainfallApiClient(HttpClient httpClient, IMapper mapper)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<RainfallReading>> GetRainfallReadingsAsync(string stationId, int count = 10)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
