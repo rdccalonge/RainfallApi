2f066fad6650ca38918d74395cdcffd8513c8a23
Reni Calonge <rdccalonge@gmail.com>
2024-01-12 23:28:33 +0800

    Create Rainfall Controller

---
+++ RainfallApi.Web/Controllers/RainfallController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RainfallApi.Core.Interfaces;
+
+namespace RainfallApi.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RainfallController : ControllerBase
+    {
+        private readonly IRainfallService _rainfallService;
+
+        public RainfallController(IRainfallService rainfallService)
+        {
+            _rainfallService = rainfallService ?? throw new ArgumentNullException(nameof(rainfallService));
+        }
+
+        [HttpGet("id/{stationId}/readings")]
+        public async Task<IActionResult> GetRainfallReadings(string stationId, [FromQuery] int count = 10)
+        {
+            try
+            {
+                var readings = await _rainfallService.GetRainfallReadingsAsync(stationId, count);
+                return Ok(readings);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
