abc0a5960a2db333b3a4d8cf95450e2fa5135e15
Reni Calonge <rdccalonge@gmail.com>
2024-01-12 17:18:34 +0800

    Define domain models

---
+++ RainfallApi.Core/ErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace RainfallApi.Core
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+        public List<ErrorDetail> Details { get; set; }
+    }
+
+    public class ErrorDetail
+    {
+        public string PropertyName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ RainfallApi.Core/RainfallReading.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainfallApi.Core
+{
+    public class RainfallReading
+    {
+        public DateTime DateMeasured { get; set; }
+        public decimal AmountMeasured { get; set; }
+    }
+}
