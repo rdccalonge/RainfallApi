ca3567faedae73644a95493f5e6639201f026fc3
Reni Calonge <rdccalonge@gmail.com>
2024-01-12 17:59:44 +0800

    Implement application service for rainfall readings.

---
+++ RainfallApi.Application/Interfaces/IRainfallService.cs
@@ -0,0 +1,9 @@
+﻿using RainfallApi.Core.Entities;
+
+namespace RainfallApi.Core.Interfaces
+{
+    public interface IRainfallService
+    {
+        Task<List<RainfallReading>> GetRainfallReadingsAsync(string stationId, int count = 10);
+    }
+}
---
+++ RainfallApi.Application/Services/RainfallService.cs
@@ -0,0 +1,18 @@
+﻿using RainfallApi.Core.Entities;
+using RainfallApi.Core.Interfaces;
+
+namespace RainfallApi.Application.Services
+{
+    public class RainfallService : IRainfallService
+    {
+        public async Task<List<RainfallReading>> GetRainfallReadingsAsync(string stationId, int count = 10)
+        {
+            var dummyData = new List<RainfallReading>
+            {
+                new RainfallReading { DateMeasured = DateTime.Now, AmountMeasured = 10.5M },
+            };
+
+            return dummyData;
+        }
+    }
+}
