950324b01b1a390eb10859fe985829901ff3dc09
Reni Calonge <rdccalonge@gmail.com>
2024-01-12 23:18:06 +0800

    Move interface to Domain.

--- RainfallApi.Core/Interfaces/IRainfallService.cs
+++ RainfallApi.Core/Interfaces/IRainfallService.cs
@@ -1,4 +1,9 @@
 ﻿using RainfallApi.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RainfallApi.Core.Interfaces
 {
