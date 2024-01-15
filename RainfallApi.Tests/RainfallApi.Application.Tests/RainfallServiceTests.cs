07b6e730f46f2b15887adaa09403857b5c8713b0
Reni Calonge <rdccalonge@gmail.com>
2024-01-15 16:51:05 +0800

    Add rainfall service tests.

--- RainfallApi.Tests/RainfallApi.Application.Tests/RainfallServiceTests.cs
+++ RainfallApi.Tests/RainfallApi.Application.Tests/RainfallServiceTests.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
 using RainfallApi.Application.Services;
 using RainfallApi.Core.Entities;
+using RainfallApi.Infrastructure.Clients;
+using RainfallApi.Infrastructure.Responses;
 using Xunit;
 
 namespace RainfallApi.Tests.RainfallApi.Application.Tests
@@ -8,17 +10,53 @@ namespace RainfallApi.Tests.RainfallApi.Application.Tests
     public class RainfallServiceTests
     {
         [Fact]
-        public async Task GetRainfallReadingsAsync_ShouldReturnReadings()
+        public async Task GetRainfallReadingsAsync_ValidStationId_ReturnsCorrectResponse()
         {
-            //// Arrange
-            //var service = new RainfallService();
+            // Arrange
+            var mockApiClient = new Mock<IRainfallApiClient>();
+            mockApiClient.Setup(x => x.GetRainfallReadingsAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new ClientDTO<ResponseDTO<RainfallReadingDTO>>
+                {
+                    IsSuccess = true,
+                    SuccessResponse = new ResponseDTO<RainfallReadingDTO>
+                    {
+                        Items = new List<RainfallReadingDTO>
+                        {
+                            new RainfallReadingDTO { DateTime = DateTime.Today, Measure = "test", Value = 15.5m }
+                        }
+                    }
+                });
 
-            //// Act
-            //List<RainfallReading> result = await service.GetRainfallReadingsAsync("stationId");
+            var rainfallService = new RainfallService(mockApiClient.Object);
 
-            //// Assert
-            //Assert.NotNull(result);
-            //Assert.Single(result);
+            // Act
+            var result = await rainfallService.GetRainfallReadingsAsync(1, 5);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Readings);
+            Assert.Single(result.Readings);
+            Assert.Equal(15.5m, result.Readings[0].AmountMeasured);
+        }
+
+        [Fact]
+        public async Task GetRainfallReadingsAsync_NotSuccess_ReturnsNull()
+        {
+            // Arrange
+            var mockApiClient = new Mock<IRainfallApiClient>();
+            mockApiClient.Setup(x => x.GetRainfallReadingsAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new ClientDTO<ResponseDTO<RainfallReadingDTO>>
+                {
+                    IsSuccess = false,
+                });
+
+            var rainfallService = new RainfallService(mockApiClient.Object);
+
+            // Act
+            var result = await rainfallService.GetRainfallReadingsAsync(1, 5);
+
+            // Assert
+            Assert.Null(result);
         }
     }
 }
